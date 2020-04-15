27c67c0e3a2cd4816c1d4c6f2ca9da8ddd1eb3b8
tessG <tess@lovegreen.life>
2020-04-15 15:46:34 +0200

    sql boilerplate with comments

--- Tournament_boilerplate.sql
+++ Tournament_boilerplate.sql
@@ -1,4 +1,6 @@
 use Turnering;
+
+/* slet eksisterende tabeller */
 SET FOREIGN_KEY_CHECKS=0; 
 DROP TABLE IF EXISTS teams;
 DROP TABLE IF EXISTS players;
@@ -8,6 +10,8 @@ DROP TABLE IF EXISTS tournaments;
 DROP TABLE IF EXISTS tournamentTeams;
 SET FOREIGN_KEY_CHECKS=1;
 
+
+/* Lav en tabel for hold */
 create table teams (
 id int NOT NULL AUTO_INCREMENT,
 name VARCHAR(40), 
@@ -16,7 +20,8 @@ position ENUM ('Winner', 'Finalist', 'Semifinalist', 'none'),
 PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
-
+/* sæt data ind i hold tabellen */
 INSERT into teams (name, position) values ('De Uovervindelige','none');
 
+/* opdater eksisterende post i hold tabellen*/
 update teams SET points = 4 WHERE id=1;
